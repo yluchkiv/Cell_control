bb311443b0288bca922d173a2b98356dfbd6ac9e
PVVovan <pvvovan@gmail.com>
2021-09-11 12:04:21 +0200

    demo LED/UART

--- main.cpp
+++ main.cpp
@@ -1,10 +1,45 @@
+#include <avr/io.h>
 #include <util/delay.h>
+
+#define BAUD	19200
+#define MYUBRR	F_CPU / 16 / BAUD - 1
+
+static void USART_Init(unsigned int ubrr)
+{
+	/*Set baud rate */
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)ubrr;
+	/* Enable receiver and transmitter */
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	/* Set frame format: 8data, 2stop bit */
+	UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+}
+
+static void USART_Transmit(unsigned char data)
+{
+	/* Wait for empty transmit buffer */
+	while (!(UCSR0A & (1<<UDRE0)))
+	;
+	/* Put data into buffer, sends the data */
+	UDR0 = data;
+}
 
 int main()
 {
-	for ( ; ; ) {
+	USART_Init(MYUBRR);
+	DDRB |= 0b0010'0000;
+
+	while (true) {
+		PORTB ^= 0b0010'0000;
 		_delay_ms(500);
+
+		static unsigned char data = '0';
+		USART_Transmit(data);
+		if (++data > '9') {
+			data = '0';
+		}
 	}
+
 	return 0;
 }
 
