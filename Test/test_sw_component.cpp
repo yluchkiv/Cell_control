6ce7709b3c3f331df6b71cb364419e1c9e708b65
PVVovan <pvvovan@gmail.com>
2021-10-16 10:24:35 +0200

    Full test coverage of sw_component

--- Test/test_sw_component.cpp
+++ Test/test_sw_component.cpp
@@ -5,3 +5,11 @@
 TEST(ComponentTest, HandlesInputOne) {
 	EXPECT_EQ(process_request(1, 1), processing_t::message);
 }
+
+TEST(ComponentTest, HandlesInputSixOne) {
+	EXPECT_EQ(process_request(6, 1), processing_t::gate);
+}
+
+TEST(ComponentTest, HandlesInputSeven) {
+	EXPECT_EQ(process_request(7, 7), processing_t::message);
+}
